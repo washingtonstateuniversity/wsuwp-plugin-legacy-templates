0af6ff5d5f44ebb6ffd1123d4e848d2fb484c549
danbleile <danbleile@gmail.com>
2020-01-17 10:51:52 -0800

    Initial Build

---
+++ wsuwp-plugin-legacy-templates.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: WSUWP Legacy Templates
+Version: 0.0.1
+Description: Add legacy template JSON feeds.
+Author: washingtonstateuniversity, Danial Bleile
+Author URI: https://github.com/washingtonstateuniversity/
+Plugin URI: https://github.com/washingtonstateuniversity/wsuwp-plugin-legacy-templates
+Text Domain: wsuwp-plugin-legacy-templates
+Requires at least: 4.7
+Tested up to: 5.2.0
+Requires PHP: 7.0
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require_once __DIR__ . '/includes/web-template.php';
